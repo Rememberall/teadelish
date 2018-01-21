7cad96596df32ffa57d402e8d71de7b0959b8ec0
Martin Lehmann <martin@lehmann.tech>
2018-01-21 16:28:18 +0100

    Initial commit: basic web layout

---
+++ web/src/mock/latest-checkins.js
@@ -0,0 +1,28 @@
+const latestCheckins = [
+  {
+    beverage: {
+      brand: 'Lipton',
+      name: 'Russian Earl Grey',
+    },
+    comment: 'SOOOO GOOD',
+    rating: 5,
+  },
+  {
+    beverage: {
+      brand: 'Twinings',
+      name: 'Darjeeling',
+    },
+    comment: 'A really long comment about how almost-perfect this tea is but not quite because I think it was a little too bitter and it didn\'t pair very well with milk because it\'s not quite strong enough',
+    rating: 4,
+  },
+  {
+    beverage: {
+      brand: 'Fortnum & Mason',
+      name: 'Smokey Earl Grey',
+    },
+    comment: 'A bit too much smoke for us',
+    rating: 2,
+  },
+];
+
+export default latestCheckins;
